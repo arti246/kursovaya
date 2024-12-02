e0fc81f9b7dd3d956ac6c68f9e11c8188954797d
Artur <artur_grishin_12345@mail.ru>
2024-12-02 21:40:43 +0700

    Add class User

---
+++ app/src/main/java/Model/User.java
@@ -0,0 +1,50 @@
+package Model;
+
+public class User {
+    private int id;
+    private String login;
+    private String password;
+
+    public static final String TABLE_NAME = "users";
+    public static final String KEY_ID_USER = "idUser";
+    public static final String KEY_LOGIN = "login";
+    public static final String KEY_PASSWORD = "password";
+
+    public User() {
+    }
+
+    public User(int id, String login, String password) {
+        this.id = id;
+        this.login = login;
+        this.password = password;
+    }
+
+    public User(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
